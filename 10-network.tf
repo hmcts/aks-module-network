b44fbaae1027c713eded91c9c9a58112e6c87a57
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-12 15:22:57 +0100

    Minor cleanup

--- 10-network.tf
+++ 10-network.tf
@@ -26,21 +26,3 @@ resource "azurerm_virtual_network" "virtual_network" {
     )
   )
 }
-
-# resource "azurerm_dns_zone" "dns_zone" {
-#   name = lookup(
-#     data.null_data_source.network_defaults.inputs, "network_internal_zonename"
-#   )
-
-#   resource_group_name = var.resource_group_name
-
-#   tags = merge(
-#     data.null_data_source.tag_defaults.inputs,
-#     map(
-#       "Name", format("%s_%s_network",
-#         lookup(data.null_data_source.network_defaults.inputs, "name_prefix"),
-#         lookup(data.null_data_source.tag_defaults.inputs, "Environment")
-#       )
-#     )
-#   )
-# }
--- 99-outputs.tf
+++
@@ -1,6 +0,0 @@
-output "network_result" {
-  value = merge(
-    data.null_data_source.network_outputs.inputs,
-    data.null_data_source.connectivity_outputs.inputs
-  )
-}
