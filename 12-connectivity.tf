8eac56740fca9f131ffda15f683783b3f1258f72
Luigi Bitonti <luigi.bitonti@hmcts.net>
2021-03-12 12:36:36 +0000

    Remove route prefix from object var

--- 12-connectivity.tf
+++ 12-connectivity.tf
@@ -86,9 +86,9 @@ resource "azurerm_route" "additional_route" {
   name                   = lower(each.value.name)
   route_table_name       = azurerm_route_table.route_table.name
   resource_group_name    = var.resource_group_name
-  address_prefix         = each.value.route_address_prefix
-  next_hop_type          = each.value.route_next_hop_type
-  next_hop_in_ip_address = each.value.route_next_hop_in_ip_address
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
+  next_hop_in_ip_address = each.value.next_hop_in_ip_address
 }
 
 resource "azurerm_subnet_route_table_association" "aks_00" {
