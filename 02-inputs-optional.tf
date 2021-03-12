bbf1a79c03f9813618dff0f6669531f62eaf6052
Luigi Bitonti <luigi.bitonti@hmcts.net>
2021-03-12 12:34:55 +0000

    DTSPO-1644 Move route to separate resource + allow additonal routes

--- 02-inputs-optional.tf
+++ 02-inputs-optional.tf
@@ -13,3 +13,14 @@ variable "route_next_hop_type" {
 variable "route_next_hop_in_ip_address" {
   default = "10.10.1.1"
 }
+
+variable "additional_routes" {
+  type = list(object({
+    name                   = string
+    address_prefix         = string
+    next_hop_type          = string
+    next_hop_in_ip_address = string
+  }))
+
+  default = []
+}
--- 12-connectivity.tf
+++ 12-connectivity.tf
@@ -69,13 +69,26 @@ resource "azurerm_route_table" "route_table" {
 
   location            = var.network_location
   resource_group_name = var.resource_group_name
+}
 
-  route {
-    name                   = var.route_name
-    address_prefix         = var.route_address_prefix
-    next_hop_type          = var.route_next_hop_type
-    next_hop_in_ip_address = var.route_next_hop_in_ip_address
-  }
+resource "azurerm_route" "default_route" {
+  name                   = var.route_name
+  route_table_name       = azurerm_route_table.route_table.name
+  resource_group_name    = var.resource_group_name
+  address_prefix         = var.route_address_prefix
+  next_hop_type          = var.route_next_hop_type
+  next_hop_in_ip_address = var.route_next_hop_in_ip_address
+}
+
+resource "azurerm_route" "additional_route" {
+  for_each = { for route in var.additional_routes : route.name => route }
+
+  name                   = lower(each.value.name)
+  route_table_name       = azurerm_route_table.route_table.name
+  resource_group_name    = var.resource_group_name
+  address_prefix         = each.value.route_address_prefix
+  next_hop_type          = each.value.route_next_hop_type
+  next_hop_in_ip_address = each.value.route_next_hop_in_ip_address
 }
 
 resource "azurerm_subnet_route_table_association" "aks_00" {
