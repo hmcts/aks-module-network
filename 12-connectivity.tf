61eb6551d080071ef8afd8a6863fc9f0fb2ab6e1
msl8r <72858053+msl8r@users.noreply.github.com>
2021-10-26 16:10:12 +0100

    DTSPO-5039 - add ability to add route table and routes to app gateway if needed

--- 12-connectivity.tf
+++ 12-connectivity.tf
@@ -59,7 +59,7 @@ resource "azurerm_subnet" "application_gateway_subnet" {
   virtual_network_name = azurerm_virtual_network.virtual_network.name
 }
 
-## VH Additional Subnets
+## Additional Subnets
 
 resource "azurerm_subnet" "additional_subnets" {
   for_each = { for subnet in var.additional_subnets : subnet.name => subnet }
@@ -84,6 +84,18 @@ resource "azurerm_route_table" "route_table" {
   tags                = var.tags
 }
 
+resource "azurerm_route_table" "appgw" {
+  count = var.appgw_routes == [] ? 0 : 1
+
+  name = format("%s-%s-appgw-route-table",
+    var.service_shortname,
+    var.environment
+  )
+
+  location            = var.network_location
+  resource_group_name = var.resource_group_name
+}
+
 resource "azurerm_route" "default_route" {
   name                   = var.route_name
   route_table_name       = azurerm_route_table.route_table.name
@@ -104,6 +116,18 @@ resource "azurerm_route" "additional_route" {
   next_hop_in_ip_address = each.value.next_hop_type != "VirtualAppliance" ? null : each.value.next_hop_in_ip_address
 }
 
+resource "azurerm_route" "additional_route_appgw" {
+  for_each = { for route in var.appgw_routes : route.name => route }
+
+  name                   = lower(each.value.name)
+  route_table_name       = azurerm_route_table.route_table_appgw.name
+  resource_group_name    = var.resource_group_name
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
+  next_hop_in_ip_address = each.value.next_hop_type != "VirtualAppliance" ? null : each.value.next_hop_in_ip_address
+
+}
+
 resource "azurerm_subnet_route_table_association" "aks_00" {
   route_table_id = azurerm_route_table.route_table.id
   subnet_id      = azurerm_subnet.aks_00_subnet.id
@@ -117,4 +141,10 @@ resource "azurerm_subnet_route_table_association" "aks_01" {
 resource "azurerm_subnet_route_table_association" "iaas" {
   route_table_id = azurerm_route_table.route_table.id
   subnet_id      = azurerm_subnet.iaas_subnet.id
+}
+
+resource "azurerm_subnet_route_table_association" "appgw" {
+  count = var.appgw_routes == [] ? 0 : 1
+  route_table_id = azurerm_route_table.route_table_appgw.id
+  subnet_id      = azurerm_subnet.application_gateway_subnet.id
 }
