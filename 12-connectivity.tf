07c4ea7c9f9aa6719163c143d5bdb675431bf3c3
Darren Bromwich <darrenb@kainos.com>
2022-04-05 14:18:16 +0100

    enabled default route for demo env

--- 12-connectivity.tf
+++ 12-connectivity.tf
@@ -84,8 +84,6 @@ resource "azurerm_route_table" "route_table" {
 }
 
 resource "azurerm_route" "default_route" {
-  count = contains(["demo"], var.environment) ? 0 : 1
-  
   name                   = var.route_name
   route_table_name       = azurerm_route_table.route_table.name
   resource_group_name    = var.resource_group_name
