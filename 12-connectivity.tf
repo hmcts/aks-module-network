13e0ad918db9ce94fc6689cd9e52e2d1f22e3aef
Thomas Thornton <>
2023-11-03 08:43:36 +0000

    no message

--- 12-connectivity.tf
+++ 12-connectivity.tf
@@ -179,7 +179,7 @@ resource "azurerm_subnet_route_table_association" "postgresql" {
   subnet_id      = azurerm_subnet.postgresql_subnet.id
 }
 
-resource "azurerm_subnet_route_table_association" "postgresql" {
+resource "azurerm_subnet_route_table_association" "postgresql_extended" {
   route_table_id = azurerm_route_table.route_table.id
   subnet_id      = azurerm_subnet.postgresql_expanded_subnet.id
 }
