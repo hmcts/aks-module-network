1b4bcafc4075f80dafad024a01482cb48d002613
Daniel Wilson <daniel.wilson@kainos.com>
2026-03-24 12:42:18 +0000

    Revert "RT & nsgs based off feat/azurerm-4.x"
    This reverts commit 21c79c976bb70443868db5ec8d12ae62c52a9bb1.

--- 02-inputs-optional.tf
+++ 02-inputs-optional.tf
@@ -28,26 +28,14 @@ variable "additional_routes" {
 variable "additional_subnets" {
   description = "Map of additional subnets to create, keyed by the subnet name."
   type = list(object({
-    name                              = string
-    address_prefix                    = string
-    service_endpoints                 = optional(list(string))
-    private_endpoint_network_policies = optional(string, "Disabled")
-    associate_route_table             = optional(bool, false)
+    name              = string
+    address_prefix    = string
+    service_endpoints = optional(list(string))
     delegations = optional(map(object({
       service_name = string,
       actions      = list(string)
     })))
-    nsg_rules = optional(list(object({
-      name                       = string
-      priority                   = number
-      direction                  = string
-      access                     = string
-      protocol                   = string
-      source_port_range          = string
-      destination_port_range     = string
-      source_address_prefix      = string
-      destination_address_prefix = string
-    })), [])
+    private_endpoint_network_policies = optional(string, "Disabled")
   }))
   default = []
 }
